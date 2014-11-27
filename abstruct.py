f0c558443fe4eb8e295e4e5ec9fb5eeefc593203
V <vlall@users.noreply.github.com>
2014-11-27 16:55:45 -0500

    Create abstruct.py

---
+++ abstruct.py
@@ -0,0 +1,19 @@
+import networkx as nx
+
+class abstruct:
+	def __init__(word1):
+		for words in bank:	
+			if (word1 == bankword):
+				return ('Success!')
+			else:
+				return ('Not a Word')
+
+	def make(wordVar)
+		wordVar = nx.Graph()
+		wordVar.add_node()
+		wordVar.add_edge(1,2)
+		print (wordVar.nodes())
+
+def main():
+	if __name__= "__main__":
+		make('hate')
