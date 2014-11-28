c9cbae5f0bf49c257403e5f27b8c6c5e2a10fc63
V <vlall@users.noreply.github.com>
2014-11-27 23:42:07 -0500

    Update abstruct.py

--- abstruct.py
+++ abstruct.py
@@ -1,15 +1,21 @@
 import networkx as nx
 
-#def main():
-#	if __name__== "__main__":
+'''
+This dictionary of nodes contains all of the weighted keys and values
+of every relevant definition node for a given word. The purpose of this is to connect
+a specific word to its abstract definition. This definition is best
+described as the group of words which are associated with word x's
 
-
+'''
 class abstruct:
 	def __init__(self,word1):
 		self.word1 = word1
-		listOfNodes = ['universe','world','earth', 'material', 'immaterial','passion'
-		'logic','emotion','mathematics','science','philosophy','existence','hate', 'love']
+		dictOfNodes = {'universe':'.3','world':'.3','earth':'.3', 'material':'.3', 'immaterial':'.3','passion':'.3',
+		'logic':'.3','emotion':'.3','mathematics':'.3','science':'.3','philosophy':'.3','existence':'.3','hate':'.3', 'love':'.2'}
+		listOfNodes = dictOfNodes.keys()
+		listOfValues = dictOfNodes.values()
 		self.listOfNodes=listOfNodes
+		self.listOfValues=listOfValues
 
 	def print_success(self):
 		if (self.word1 in self.listOfNodes):
@@ -21,8 +27,10 @@ class abstruct:
 		#node names
 		wordVar = nx.Graph()
 		wordVar.add_nodes_from(self.listOfNodes)
+		weightNum = self.listOfValues
+		print weightNum
 		for i in range(len(self.listOfNodes)):
-			wordVar.add_edge(wordVar,self.listOfNodes[i], weight = .3)
+			wordVar.add_edge(wordVar,self.listOfNodes[i], weight = weightNum[i])
 		return (wordVar.nodes())
 
 x = abstruct('hate')
