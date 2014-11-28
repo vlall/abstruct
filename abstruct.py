91313665ebe93e44314bf797394492a413bd17a3
V <vlall@users.noreply.github.com>
2014-11-27 21:42:45 -0500

    Update abstruct.py

--- abstruct.py
+++ abstruct.py
@@ -1,25 +1,29 @@
 import networkx as nx
 
-def main():
-	if __name__== "__main__":
-		make('hate')
+#def main():
+#	if __name__== "__main__":
 
 
 class abstruct:
-	def __init__(self, word1):
+	def __init__(self,word1):
+		self.word1 = word1
 		listOfNodes = ['universe','world','earth', 'material', 'immaterial','passion'
 		'logic','emotion','mathematics','science','philosophy','existence','hate', 'love']
-		if (word1 in listOfNodes):
+		self.listOfNodes=listOfNodes
+
+	def print_success(self):
+		if (self.word1 in self.listOfNodes):
 			return ('Success!')
 		else:
 			return ('Not a Word')
 
-	def make(wordVar):
+	def make(self, wordVar):
 		#node names
 		wordVar = nx.Graph()
-		wordVar.add_nodes_from(listOfNodes)
+		wordVar.add_nodes_from(self.listOfNodes)
 		wordVar.add_edge('logic','subject', weight = .3)
-		print (wordVar.nodes())
-
+		return (wordVar.nodes())
 
 x = abstruct('hate')
+print x.print_success()
+print x.make('hate')
